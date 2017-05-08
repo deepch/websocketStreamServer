0edb822e9b9c598e7ea656eee10bc4ce18ffb35f
FgoDt <fgodtdev@gmail.com>
2017-05-08 14:08:24 +0800

    delete backend server function

--- backend/backendService.go
+++ backend/backendService.go
@@ -7,9 +7,12 @@ import (
 	"encoding/json"
 	"strconv"
 	"net/http"
-	"crypto/md5"
-	"encoding/hex"
 )
+
+type BackendHander interface {
+	Init (msg *wssAPI.Msg) error
+	GetRoute()string
+}
 
 type  BackendService struct {
 }
@@ -18,10 +21,8 @@ type BackendConfig struct {
 	Port	int	`json:"Port"`
 	RootName string `json:"Usr"`
 	RootPwd string `json:"Pwd"`
-	ActionToken string 
 }
 
-var service *BackendService
 var serviceConfig BackendConfig
 
 func (this *BackendService) Init(msg *wssAPI.Msg) (err error){
@@ -36,12 +37,16 @@ func (this *BackendService) Init(msg *wssAPI.Msg) (err error){
 		logger.LOGE(err.Error())
 		return errors.New("load backend config failed")
 	}
-	service = this
+
+
 
 	go func() {
 		strPort := ":" + strconv.Itoa(serviceConfig.Port)
-		//http.Handle("/admin",http.StripPrefix("/admin",this))
-		http.Handle("/admin/login",http.StripPrefix("/admin/login",this))
+		handlers := backendHandlerInit()
+		for _, item := range handlers{
+			backHandler := item.(BackendHander)
+			http.Handle(backHandler.GetRoute(),http.StripPrefix(backHandler.GetRoute(),backHandler.(http.Handler)))
+		}
 		err = http.ListenAndServe(strPort,nil)
 		if err != nil {
 			logger.LOGE("start backend serve failed")
@@ -63,12 +68,7 @@ func (this *BackendService) loadConfigFile(fileName string) (err error)  {
 	return
 }
 
-//handle
-func (this *BackendService) ServeHTTP(w http.ResponseWriter,req *http.Request){
-	if req.RequestURI == "/admin/login"{
-		this.HandleLoginRequest(w,req)
-	}
-}
+
 
 func (this *BackendService)Start(msg *wssAPI.Msg) (err error){
 	return
@@ -91,66 +91,31 @@ func (this *BackendService) ProcessMessage(msg *wssAPI.Msg) (err error) {
 	return
 }
 
-func (this *BackendService) HandleLoginRequest(w http.ResponseWriter, req *http.Request){
-	if req.Method != "POST"{
-		result, err := badRequest(1,"bad request")
-		SendResponse(result,err,w)
-	}else{
-		username := req.PostFormValue("username")
-		password := req.PostFormValue("password")
-		if len(username)>0 && len(password)>0 {
-			ispass, authToken := compAuth(username,password)
-			if ispass{
-				serviceConfig.ActionToken = authToken
-				responseData, err := passAuthResponseData(authToken)
-				SendResponse(responseData,err,w)
-			}else{
-				responseData, err := badRequest(2,"login auth error")
-				SendResponse(responseData,err,w)
-			}
-		}else{
-			responseData, err := badRequest(2,"login auth error")
-			SendResponse(responseData,err,w)
+
+
+func backendHandlerInit()([]BackendHander){
+	handers := make([]BackendHander,0)
+
+	adminLoginHandle := &AdminLoginHandler{}
+	lgData := &wssAPI.Msg{}
+	loginData := AdminLoginData{}
+	loginData.password = serviceConfig.RootPwd
+	loginData.username = serviceConfig.RootName
+	lgData.Param1 = loginData
+	err :=adminLoginHandle.Init(lgData)
+	if err == nil {
+		handers = append(handers,adminLoginHandle)
+	}else {
+		if err != nil {
+		logger.LOGE("add adminLoginHandle error!")
 		}
 	}
+
+	streamManagerHandle := &AdminStreamManageHandler{}
+	streamManagerHandle.Init(nil)
+	handers = append(handers,streamManagerHandle)
+
+	return handers
 }
 
-func SendResponse(responseData []byte,err error,w http.ResponseWriter){
-	if err != nil{
-		w.Write([]byte("sevr error!"))
-	}else{
-		w.Write(responseData)
-	}
-}
 
-//login sucess response 
-func passAuthResponseData(authToken string)([]byte,error){
-	result := &ResponseData{}
-	result.Code = 200
-	result.Msg = "ok"
-	result.Data.UserData.Token = authToken
-	result.Data.UserData.Usrname = serviceConfig.RootName
-
-	resultData, err := json.Marshal(result)
-	return resultData, err
-}
-
-func compAuth(username,password string)(ispass bool,authToken string){
-	if serviceConfig.RootName == username && serviceConfig.RootPwd == password{
-		hash := md5.New()
-		hash.Write([]byte(username+password))
-		md5data := hash.Sum(nil)
-		md5str := hex.EncodeToString(md5data)
-		return true,md5str
-	}else{
-		return false,""
-	}
-}
-
-func badRequest(code int,msg string)( []byte, error){
-	result := &ResponseData{}
-	result.Code = code
-	result.Msg = msg
-	bytes, err := json.Marshal(result)
-	return bytes , err
-}
